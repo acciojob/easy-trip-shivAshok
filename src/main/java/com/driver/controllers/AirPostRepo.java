9d865091b816e3935b4f02328e9299a8bb0ac59c
Shiv mishra <sm096951@gmail.com>
2023-07-01 22:49:39 +0530

    check it

---
+++ src/main/java/com/driver/controllers/AirPostRepo.java
@@ -0,0 +1,94 @@
+package com.driver.controllers;
+
+import com.driver.model.Airport;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+
+import java.util.*;
+
+public class AirPostRepo {
+    Map<String,Airport> Airports=new HashMap<>();
+    Map<Integer,Flight> flights=new HashMap<>();
+    Map<Integer,Passenger> passengers=new HashMap<>();
+    Map<Integer,List<Integer>> FlightwithPassenger=new HashMap<>();
+    Map<Integer,Integer> flightWithRevenue=new HashMap<>();
+    public void addTheAirport(Airport airport) {
+        Airports.put(airport.getAirportName(),airport);
+    }
+
+    public List<Airport> getAllairPorts() {
+        return new ArrayList<>(Airports.values());
+    }
+
+    public void addFlight(Flight flight) {
+        flights.put(flight.getFlightId(),flight);
+    }
+
+    public List<Flight> getAllflights() {
+        return new ArrayList<>(flights.values());
+    }
+
+    public void addPassenger(Passenger passenger) {
+        passengers.put(passenger.getPassengerId(),passenger);
+    }
+
+    public Optional<Airport> getAirportByname(String airportName) {
+        if(Airports.containsKey(airportName)){
+            return Optional.of(Airports.get(airportName));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Flight> getFlightbyFlightId(Integer flightId) {
+        if(flights.containsKey(flightId)){
+            return Optional.of(flights.get(flightId));
+        }
+        return Optional.empty();
+    }
+
+    public String BookAteicket(Integer flightId, Integer passengerId) {
+        if(FlightwithPassenger.containsKey(flightId)){
+            List<Integer> passengers=FlightwithPassenger.get(flightId);
+             if(passengers.size()==this.getFlightbyFlightId(flightId).get().getMaxCapacity()){
+                 return "FAILURE";
+             }
+               for(int passenger:passengers ){
+                   if(passenger==passengerId){
+                       return "FAILURE";
+                   }
+            }
+            int revenue=flightWithRevenue.get(flightId)+3000*passengers.size();
+               flightWithRevenue.put(flightId,revenue);
+               passengers.add(passengerId);
+               FlightwithPassenger.put(flightId,passengers);
+
+        }
+        else{
+            List<Integer> passengers=new ArrayList<>();
+            passengers.add(passengerId);
+            FlightwithPassenger.put(flightId,passengers);
+            flightWithRevenue.put(flightId,3000);
+        }
+        return "SUCCESS";
+    }
+
+
+    public List<Integer> getAllPassengerByflightId(Integer flightId) {
+        return FlightwithPassenger.getOrDefault(flightId,new ArrayList<>());
+    }
+
+    public void cancelTicket(Integer flightId, Integer passengerId) {
+        List<Integer> passengers=FlightwithPassenger.get(flightId);
+        passengers.remove(passengerId);
+        int revenue=flightWithRevenue.get(flightId)-(passengers.size()-1)*50;
+        flightWithRevenue.put(flightId,revenue);
+    }
+
+    public List<Integer> getAllflightIds() {
+        return new ArrayList<>(flights.keySet());
+    }
+
+    public int getRevenue(Integer flightId) {
+        return flightWithRevenue.getOrDefault(flightId,0);
+    }
+}
---
+++ src/main/java/com/driver/controllers/AirportService.java
@@ -0,0 +1,137 @@
+package com.driver.controllers;
+
+import com.driver.model.Airport;
+import com.driver.model.City;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public class AirportService {
+    @Autowired
+    private AirPostRepo airrepo;
+    public void AddairPort(Airport airport) {
+        airrepo.addTheAirport(airport);
+    }
+
+    public String getLargestAirportName() {
+        List<Airport> airports=airrepo.getAllairPorts();
+        int maxterminal=0;
+        String largestAirport="";
+        for(Airport airport:airports){
+
+            if(airport.getNoOfTerminals()>maxterminal){
+                maxterminal=airport.getNoOfTerminals();
+                largestAirport=airport.getAirportName();
+            }
+            else if(airport.getNoOfTerminals()==maxterminal && airport.getAirportName().compareTo(largestAirport)>0){
+                largestAirport=airport.getAirportName();
+            }
+        }
+        return largestAirport;
+    }
+
+    public void addFlight(Flight flight) {
+        airrepo.addFlight(flight);
+    }
+
+    public double getShortestDurationOfPossibleBetweenTwoCities(City fromCity, City toCity) {
+        List<Flight> flights=airrepo.getAllflights();
+        double duration=Double.MAX_VALUE;
+        for(Flight flight:flights){
+            if(flight.getFromCity()==fromCity && flight.getToCity()==toCity){
+                duration=Math.min(duration,flight.getDuration());
+            }
+        }
+        if(duration==Double.MAX_VALUE){
+            return -1;
+        }
+
+        return duration;
+    }
+
+    public void addPassenger(Passenger passenger) {
+        airrepo.addPassenger(passenger);
+    }
+
+    public int getNoOfPeople(Date date, String airportName) {
+        Optional<Airport> airportOpt=airrepo.getAirportByname(airportName);
+        if(airportOpt.isEmpty()){
+            throw new RuntimeException("Airport is not present");
+        }
+        List<Flight> flights=airrepo.getAllflights();
+        int numOfpeople=0;
+        for(Flight flight:flights){
+            if(flight.getFlightDate().equals(date) && (flight.getToCity().equals(airportOpt.get().getCity())||flight.getToCity().equals(airportOpt.get().getCity()))) {
+                numOfpeople += flight.getMaxCapacity();
+            }
+        }
+        return numOfpeople;
+    }
+
+    public String getAirPortnamefromFlightId(Integer flightId) {
+        Optional<Flight> flightOpt=airrepo.getFlightbyFlightId(flightId);
+        if(flightOpt.isEmpty()){
+            return null;
+        }
+        City city=flightOpt.get().getFromCity();
+        List<Airport> airports=airrepo.getAllairPorts();
+        for(Airport airport:airports){
+            if(airport.getCity().equals(city)){
+                return airport.getAirportName();
+            }
+        }
+        return null;
+    }
+
+    public String bookATicket(Integer flightId, Integer passengerId) {
+        return airrepo.BookAteicket(flightId,passengerId);
+    }
+
+    public String cancelTicket(Integer flightId, Integer passengerId) {
+        if(airrepo.getFlightbyFlightId(flightId).isEmpty()){
+            return "FALIURE";
+        }
+        List<Integer> passenger=airrepo.getAllPassengerByflightId(flightId);
+        boolean ispresent=false;
+        for(int p:passenger){
+            if(p==passengerId){
+                ispresent=true;
+            }
+        }
+        if(ispresent){
+            airrepo.cancelTicket(flightId,passengerId);
+            return "SUCCESS";
+        }
+        else{
+            return "FALIURE";
+        }
+    }
+
+    public int getCountOfbookingsdonebyPassenger(Integer passengerId) {
+        List<Integer> flightId=airrepo.getAllflightIds();
+        int count=0;
+        for(int Id:flightId){
+            List<Integer> passengers=airrepo.getAllPassengerByflightId(Id);
+            for(int P:passengers){
+                if(P==passengerId){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int calculateFair(Integer flightId) {
+        List<Integer> passengers=airrepo.getAllPassengerByflightId(flightId);
+        int fare=3000+50*passengers.size();
+        return fare;
+    }
+
+    public int calculateRevenue(Integer flightId) {
+        return airrepo.getRevenue(flightId);
+    }
+}
